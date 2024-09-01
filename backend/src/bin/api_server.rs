5c419c9ba1e608ce0587cf2652a0cfc03286fc92
VYAR <vadim.d.yarovoy@yandex.ru>
2024-09-01 17:33:41 +0300

    feat: Provide db pool

--- backend/src/bin/api_server.rs
+++ backend/src/bin/api_server.rs
@@ -5,9 +5,10 @@ use backend::AppConfig;
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     tracing_subscriber::fmt::init();
+
     let config: AppConfig = AppConfig::figment().extract()?;
     let addr = config.server.bind_addr();
-    let app = backend::app().await;
+    let app = backend::app(config).await;
     info!("SERVER: Starting");
 
     let listener = tokio::net::TcpListener::bind(&addr).await?;
