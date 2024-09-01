6efb0b831e2579e369f55ea3c18dcfffc903dd93
VYAR <vadim.d.yarovoy@yandex.ru>
2024-09-01 18:00:06 +0300

    feat: Move from lambdas to functions

--- backend/src/api/orders.rs
+++ backend/src/api/orders.rs
@@ -1,10 +1,18 @@
 use axum::{routing, Router};
 
+async fn get_order_by_id() -> &'static str {
+    "GET REQUEST"
+}
+
+async fn create_order() -> &'static str {
+    "POST REQUEST"
+}
+
 pub fn routes<S>() -> Router<S>
 where
     S: Send + Sync + Clone + 'static,
 {
     Router::new()
-        .route("/get/:id", routing::get(|| async { "GET REQUEST" }))
-        .route("/create", routing::post(|| async { "POST REQUEST" }))
+        .route("/get/:id", routing::get(get_order_by_id))
+        .route("/create", routing::post(create_order))
 }
