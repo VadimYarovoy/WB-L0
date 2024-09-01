cd0fde84fe1756490214cdd44758009277dc98a3
VYAR <vadim.d.yarovoy@yandex.ru>
2024-09-01 18:13:46 +0300

    feat: Complete get order by id handler

--- backend/src/api/orders.rs
+++ backend/src/api/orders.rs
@@ -1,9 +1,28 @@
 use crate::AppState;
-use axum::{extract::State, routing, Router};
+use axum::{extract::{Path, State, Json}, http::StatusCode, response::{IntoResponse, Response}, routing, Router};
+use tracing::info;
 use std::sync::Arc;
 
-async fn get_order_by_id(State(_state): State<Arc<AppState>>) -> &'static str {
-    "GET REQUEST"
+async fn get_order_by_id(Path(id): Path<i32>, State(state): State<Arc<AppState>>) -> Response {
+    let client = state.pool.get().await.unwrap();
+
+    let row = client
+        .query_one("SELECT order_data FROM orders WHERE id = $1", &[&id])
+        .await;
+
+    match row {
+        Ok(row) => {
+            info!("Take value with id = {} from db", id);
+            let order: serde_json::Value = row.get(0);
+
+            (StatusCode::OK, Json(order)).into_response()
+        }
+        _ => (
+            StatusCode::NOT_FOUND,
+            format!("ERROR: There is no order with id = {id}"),
+        )
+            .into_response(),
+    }
 }
 
 async fn create_order(State(_state): State<Arc<AppState>>) -> &'static str {
